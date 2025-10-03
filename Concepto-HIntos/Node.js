c86aab7a3b0d142f04101a256f8c5c38956fc1bb
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-10-03 12:41:55 +0530

    Create Node.js

---
+++ Concepto-HIntos/Node.js
@@ -0,0 +1,7 @@
+1. Node.js
+
+Q: Is Node.js a programming language or runtime?
+A: Runtime environment for executing JavaScript outside the browser.
+
+Q: Why do we need Node.js for React apps?
+A: It runs build tools (webpack, babel) and package manager (npm), making development possible.
